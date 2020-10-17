748e8fd0c7a7961a955be1b2fcb339d08c90da81
Sayid Abyan Rizal Shiddiq <sayidabyan@gmail.com>
2020-10-17 22:29:37 +0700

    allowed host fixed

--- help_budi/settings.py
+++ help_budi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'dkgv#kux(3kfb*=hgkv&v)b&r5pe9p7^dhqsjz1pv3g6h9f9as'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
